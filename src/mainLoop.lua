d7a74956575faef5b7f9487f35a52d6ddc2351c7
Manuel Pascual <pascual.manuel@gmail.com>
2016-03-23 10:04:26 +0100

    control watchdog

--- src/mainLoop.lua
+++ src/mainLoop.lua
@@ -10,7 +10,7 @@
 --[[----- NO CAMBIAR EL CODIGO A PARTIR DE AQUI ------------------------------]]
 
 --[[----- CONFIGURACION AVANZADA ---------------------------------------------]]
-local release = {name='ControlConsumoElect.mainLoop', ver=2, mayor=1, minor=1}
+local release = {name='ControlConsumoElect.mainLoop', ver=2, mayor=1, minor=0}
 cceEstado = 'cceEstado'     -- nombre variable global para almacenar el estado
 cceConsumo = 'cceConsumo'   -- nombre variable global para almacenar consumos
 OFF=1;INFO=2;DEBUG=3                -- referencia para el log
@@ -33,15 +33,20 @@ end
 _log(INFO, release['name']..
 ' ver '..release['ver']..'.'..release['mayor']..'.'..release['minor'])
 
+local contador = 0
 --[[--------BUCLE DE CONTROL -------------------------------------------------]]
+local tablaEstado
 while true do
   local mensaje = ''
   fibaro:log(mensaje)
   -- obtener mesaje de estado
-  local tablaEstado = json.decode(fibaro:getGlobalValue(cceEstado))
-  mensaje = tablaEstado.mensaje..' - '..tablaEstado.preciokwh..'€/kWh'
+  tablaEstado = json.decode(fibaro:getGlobalValue(cceEstado))
+  local mensaje = tablaEstado.mensaje..' - '..tablaEstado.preciokwh..'€/kWh'
   -- referscar log
   fibaro:log(mensaje)
+  -- parar 1 seg. para evitar problemas de rendimiento
   fibaro:sleep(1000)
+  -- notificación de estado para watchdog
+  fibaro:debug('ControlConsumo OK')
 end
 --[[--------------------------------------------------------------------------]]
